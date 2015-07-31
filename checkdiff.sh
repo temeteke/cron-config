76413eeb2b190d91b8ae6d7a3fdb9117ce078f43
temeteke <temeteke@gmail.com>
2015-07-31 09:40:43 +0900

    Fix checkdiff.sh to work when | is used

--- checkdiff.sh
+++ checkdiff.sh
@@ -6,6 +6,6 @@ mkdir -p $DIR && cd $DIR
 cmd=$*
 file=$(echo "$cmd" | md5sum | awk '{print $1}')
 
-$cmd > /tmp/$file
+eval $cmd > /tmp/$file
 [ -f $file ] && diff $file /tmp/$file
 mv /tmp/$file $file
